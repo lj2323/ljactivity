c73ccbc9010ca0db7e2ef5d0443ca06017ad0cf5
lj2323 <77177786+lj2323@users.noreply.github.com>
2021-01-10 14:46:18 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Socket master = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPEndPoint ipEnd = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 8888);
+            master.Connect(ipEnd);
+
+            string sendData = "";
+
+            do
+            {
+                Console.Write("Send Message: ");
+                sendData = Console.ReadLine();
+                master.Send(System.Text.Encoding.UTF8.GetBytes(sendData));
+                byte[] pbd = new byte[4];
+                master.Receive(pbd);
+                Console.WriteLine("Server Response: " + System.Text.Encoding.UTF8.GetString(pbd));
+            } while (sendData.Length > 0);
+
+            master.Close();
+        }
+    }
+}
